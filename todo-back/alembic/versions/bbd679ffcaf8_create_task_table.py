473f02dd0237b200ad4c2fb32cd56f64ba4e4905
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-01-23 12:02:35 +0200

    todo api ready - fastapi

---
+++ todo-back/alembic/versions/bbd679ffcaf8_create_task_table.py
@@ -0,0 +1,24 @@
+"""create task table
+
+Revision ID: bbd679ffcaf8
+Revises: 
+Create Date: 2021-01-23 11:33:07.661235
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bbd679ffcaf8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
