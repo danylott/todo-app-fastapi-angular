66ad33be6ce0ce01d48908c60ad7abc89fa442b5
d.tuytuyshkin@gmail.com <d.tuytuyshkin@gmail.com>
2021-01-23 12:01:30 +0200

    added api support for fetching data

--- todo-front/src/app/task.service.ts
+++ todo-front/src/app/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Task } from './interfaces/task';
 import { TASKS } from './mocks/tasks';
@@ -9,18 +10,24 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class TaskService {
+  private taskUrl = 'http://127.0.0.1:8000/tasks/';
+
+  // tslint:disable-next-line:typedef
+  private log(message: string) {
+    this.messageService.add(`TaskService: ${message}`);
+  }
 
   getTasks(): Observable<Task[]> {
-    this.messageService.add('TaskService: fetched tasks');
-    return of(TASKS);
+    this.log('fetched tasks');
+    return this.http.get<Task[]>(this.taskUrl);
   }
 
   getTask(id: number): Observable<Task> {
     // TODO: send the message _after_ fetching the task
-    this.messageService.add(`TaskService: fetched task id=${id}`);
+    this.log(`fetched task id=${id}`);
     // @ts-ignore
-    return of(TASKS.find(task => task.id === id));
+    return this.http.get<Task[]>(`${this.taskUrl}${id}/`);
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 }
